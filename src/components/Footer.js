2dbbe5d0bda85223f62d26f8b8f53b17da946a96
Nehach27 <nehachoudhary073@gmail.com>
2019-03-16 12:35:04 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer">
+            <h2>Thank you</h2>
+            <h3>for your time!</h3>
+          
+            <a href="#base"><img src={require("../Images/up.png")} alt="Thank you image" /></a>
+            <p>go back</p>
+    </div>
+  )
+}
+
+export default Footer
